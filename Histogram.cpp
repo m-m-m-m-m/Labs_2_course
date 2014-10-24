bef37e80456870afeaa07109c0187179a5cb0af8
Slayv <Slayv@gala.net>
2014-10-24 21:22:29 +0300

    Update Histogram.cpp

--- Histogram.cpp
+++ Histogram.cpp
@@ -10,24 +10,23 @@ using namespace std;
 
 int main ()
 {
-FILE * file;
-int i;
-char letter, a=0;
+	FILE * file;
+	int i;
+	char letter, a=0;
+	int* histogram = new int[256];
+	for (int i=0;i<256;i++)	histogram[i] = 0;
 
-int* histogram = new int[256];
-for (int i=0;i<256;i++)	histogram[i] = 0;
+	file = fopen("1.txt", "rb");
+	for(i=0; fscanf(file, "%c", &letter) != EOF; i++) histogram[letter]++;
+	fclose(file);
 
-file = fopen("1.txt", "rb");
-for(i=0; fscanf(file, "%c", &letter) != EOF; i++) histogram[letter]++;
-fclose(file);
+	for (i=0; i<256; i++)
+	{
+		cout  << "\""<< a << "\"\t" << histogram[i] << endl;
+		a++;
+	}
 
-for (i=0; i<256; i++)
-{
-	cout  << "\""<< a << "\"\t" << histogram[i] << endl;
-	a++;
-}
-
-system("pause");
-return 0;
+	system("pause");
+	return 0;
 }
 
